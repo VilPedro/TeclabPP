cf37dae5d88e050bc2f101060ee27c31d52ad259
Pedro <pedro.len@hotmail.com>
2024-07-28 19:01:51 -0300

    Pagina de inicio completa salvo unos enlaces

--- index.js
+++ index.js
@@ -9,58 +9,46 @@ const port = 3000
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const CLIENT_ID = '9zi4dvcfg0asp0eukw4h9prfg0zjfn'
-const ACCESS_TOKEN = 'ig5f5ywfoc0nj799b1ompa872q17mk'
+const apiKey = 'fcff05fc4cec4cb1a44a5132c43b9065'
+const baseUrl = 'https://api.rawg.io/api/games'
+const delay = 1000
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(path.resolve(), 'views'))
-
-express.static.mime.define({ 'text/css': ['css'] })
-express.static.mime.define({ 'application/javascript': ['js'] })
-express.static.mime.define({ 'image/jpeg': ['jpg', 'jpeg'] })
-express.static.mime.define({ 'image/png': ['png'] })
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 
+const fetchGamesByCategoryWithDelay = async (category, pages) => {
+  const games = []
+  for (const page of pages) {
+    try {
+      const response = await axios.get(`${baseUrl}?key=${apiKey}&genres=${category}&ordering=+rating&page_size=16&page=${page}`)
+      games.push(...response.data.results)
+      await new Promise(resolve => setTimeout(resolve, delay))
+    } catch (error) {
+      console.error(`Error fetching ${category} games:`, error)
+    }
+  }
+  return games
+}
+
 app.get('/', async (req, res) => {
   try {
-    const response = await axios.get(
-      'https://api.igdb.com/v4/games',
-      {
-        headers: {
-          Accept: 'application/json',
-          'Client-ID': CLIENT_ID,
-          Authorization: `Bearer ${ACCESS_TOKEN}`
-        },
-        params: {
-          fields: 'id,name,cover.image_id',
-          sort: 'popularity desc'
-        }
-      }
-    )
+    const actionGames = await fetchGamesByCategoryWithDelay('action', [1, 2, 3])
+    const adventureGames = await fetchGamesByCategoryWithDelay('adventure', [1, 2, 3])
+    const racingGames = await fetchGamesByCategoryWithDelay('racing', [1, 2, 3])
 
-    // Procesar la respuesta para obtener los hashes de las imágenes y construir las URLs
-    const games = response.data.map(game => {
-      if (game.cover && game.cover.image_id) {
-        const imageHash = game.cover.image_id
-        const imageUrl = `https://images.igdb.com/igdb/image/upload/t_cover_big/${imageHash}.jpg`
-        return {
-          name: game.name,
-          imageUrl
-        }
-      }
-      return game
-    })
-
-    // Renderizar la plantilla EJS y pasar los datos de los juegos
-    res.render('index', { games })
+    res.render('index', { actionGames, adventureGames, racingGames, error: null })
   } catch (err) {
-    console.error('Error:', err.response ? err.response.data : err.message)
-    res.status(500).send('Error al obtener los datos de los juegos')
+    console.error('Error:', err)
+    res.render('index', { actionGames: [], adventureGames: [], error: 'Error al obtener datos de los juegos' })
   }
 })
 
+app.get('/about', async (req, res) => {
+  res.render('about')
+})
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
 })
